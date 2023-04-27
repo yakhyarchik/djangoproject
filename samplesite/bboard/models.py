39875d2abfa230f1ea3a2f8859156ce8e3e9ad06
tamerlan <tamerlan.yakhyarov@mail.ru>
2023-04-27 17:30:42 +0600

    lesson_03

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,11 +1,37 @@
 from django.db import models
 
 
+class Rubric(models.Model):
+    name = models.CharField(
+        max_length=20,
+        db_index=True,
+        verbose_name='Название',
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Рубрика'
+        verbose_name_plural = 'Рубрики'
+        ordering = ['name']
+
+
 class Bb(models.Model):
+    rubric = models.ForeignKey(
+        'Rubric',
+        null=True,
+        on_delete=models.PROTECT,
+        verbose_name='Рубрика',
+    )
+
     title = models.CharField(max_length=50, verbose_name='Товар')
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = 'Объявление'
